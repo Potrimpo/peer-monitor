6b141009be4fd2f7d88b1d05c0dae679b72d7e23
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2017-07-12 19:41:50 +1200

    nodes are now placed in map under key = infohash

--- lib/indexer.ex
+++ lib/indexer.ex
@@ -14,39 +14,44 @@ defmodule Indexer do
   def init([]) do
     :timer.sleep(1000) # Give MLDHT time to setup before calling it
     :ok = begin_dht_crawl()
-    {:ok, []}
+    {:ok, %{}}
   end
 
-  def handle_cast({:post_node, node}, state) do
+  def handle_cast({:post_node, {hash, node}}, state) do
     IO.puts "new node! --> #{inspect node}"
-
-    {:noreply, [node | state]}
+    new_state = Map.update(state, hash, [node], fn xs -> [node | xs] end)
+    {:noreply, new_state}
   end
 
   # PRIVATE FUNCTIONS
 
   defp begin_dht_crawl() do
     _oks = Crawler.magnets
-      |> Magnet.get(:xt) # take the content hash
-      |> Enum.take(1) # just testing
-      |> Enum.map(&searcher/1)
+    |> Magnet.get(:xt) # take the content hash
+    |> Enum.take(1) # just testing
+    |> Enum.map(&parse_and_search/1)
 
-    IO.puts "DHT crawl has begun"
     :ok
   end
 
-  defp searcher("urn:btih:" <> hash) do
+  defp parse_and_search("urn:btih:" <> hash) do
     hash
     |> String.upcase
     |> Base.decode16!
-    |> MLDHT.search(&search_callback/1)
+    |> dht_search
   end
 
-  defp search_callback(node), do: GenServer.cast(@name, {:post_node, node})
+  defp dht_search(hash) do
+    MLDHT.search(hash, fn node -> search_callback(hash, node) end)
+  end
+
+  defp search_callback(hash, node), do: GenServer.cast(@name, {:post_node, {hash, node}})
 
 end
 
 # "b99f93d2df9472910941c4a315718fb0d1eff191" \
 # |> String.upcase \
 # |> Base.decode16! \
-# |> MLDHT.search(fn node -> IO.puts "new node! --> #{inspect node}" end)
+# |> MLDHT.search(fn node -> \
+#     GenServer.cast(Indexer, {:post_node, node}) \
+#   end)
